5829e738c7b57abf962112dd30fa1706c3bc65ff
PanuwatSF <panuwat200527@gmail.com>
2024-11-27 11:01:01 +0700

    lab101

---
+++ LabSheet01/src/Lab101.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Lab101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input product name : ");
+		String productName = input.nextLine();
+		System.out.print("Input product unit : ");
+		int productUnit = input.nextInt();
+		System.out.print("Input product per unit : ");
+		double productPrice = input.nextFloat();
+		System.out.println("------------------------------------");
+		double totalPrice = productUnit * productPrice;
+		String frmTotalPrice = String.format("%,.2f",totalPrice);
+		System.out.println("Total Price is "+ frmTotalPrice +" baht.");
+		System.out.println("------------------------------------");
+		System.out.print("How many dicount (%) : ");
+		int discount = input.nextInt();
+		System.out.println("------------------------------------");
+		double totalDiscount = totalPrice * (discount/100.0);
+		double amount = totalPrice - totalDiscount;
+		String frmTotalDiscount = String.format("%,.2f",totalDiscount);
+		String frmAmount = String.format("%,.2f",amount);
+		System.out.println("Discount for "+discount+"%\t"+ frmTotalDiscount + " baht.");
+		System.out.println("Amount to be paid \t"+ frmAmount+" bath.");
+	}
+
+}
